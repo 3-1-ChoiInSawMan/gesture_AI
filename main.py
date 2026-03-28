f64b919de8b59329e9dbaa4528eade79e3161181
yeeeengyu <chldlsrb08@naver.com>
2026-03-28 12:04:59 +0900

    feature: 라우터추가

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from router import health, cc
+from router import health, cc, cc_stt
 
 
 APP = FastAPI()
 APP.include_router(health.router)
 APP.include_router(cc.router)
+APP.include_router(cc_stt.router)
