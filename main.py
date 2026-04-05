0b347864714338bbebd1e7f456c9576e139320a3
yeeeengyu <chldlsrb08@naver.com>
2026-04-06 08:03:40 +0900

    chore: summary 라우터추가

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from router import health, cc, cc_stt
+from router import health, cc, cc_stt, summary
+from pymongo import MongoClient
 
 APP = FastAPI()
 APP.include_router(health.router)
 APP.include_router(cc.router)
 APP.include_router(cc_stt.router)
+APP.include_router(summary.router)
