e1b37e7ea644f9a06b4aab61178683e51552b4b3
yeeeengyu <chldlsrb08@naver.com>
2026-04-07 16:30:07 +0900

    feature: 모델 연동

--- router/cc.py
+++ router/cc.py
@@ -1,9 +1,16 @@
 import os
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from openai import OpenAI
+import torch
+import numpy as np
+
 
 router = APIRouter(prefix='/ai')
 Client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = torch.load('best_bigru.pt', map_location=device)
+model.to(device)
 
 WINDOW_SIZE = 60
 
@@ -13,10 +20,18 @@ async def jamak(websocket: WebSocket):
     buffer = []
     try:
         while True:
-            chunk = await websocket.receive_bytes()
-            buffer.append(chunk)
-
+            data = await websocket.receive_json()
+            keys = data['keypoints']
             if len(buffer) >= WINDOW_SIZE:
-                    await websocket.send_text()
+                sequence = np.array(buffer[-WINDOW_SIZE:], dtype= np.float32)
+                x = torch.tensor(sequence, dtype=torch.float32).unsqueeze(0).to(device)
+
+                with torch.no_grad():
+                    out = model(x)
+                    pred = torch.argmax(out, dim = 1).item()
+
+                await websocket.send_text(str(pred))
+                buffer = []
+
     except WebSocketDisconnect:
         pass
