b90a6c4054722d85dc0c7c8703e70be56df1f9a3
yeeeengyu <chldlsrb08@naver.com>
2026-04-09 16:06:25 +0900

    feature: 자막 읽어주기

--- router/cc_stt.py
+++ router/cc_stt.py
@@ -1,10 +1,22 @@
 import io
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from faster_whisper import WhisperModel
+import torch
 
 router = APIRouter()
+model = WhisperModel("small", device = 'cuda' if torch.cuda.is_available() else 'cpu')
 
 WINDOW_SIZE = 15 
 
 @router.websocket('/cc_stt')
-async def stt_cc():
-    pass
+async def stt_cc(ws: WebSocket):
+    await ws.accept()
+    try:
+        while True:
+            data = await ws.receive_bytes()
+            audio = io.BytesIO(data)
+            segments, info = model.transcribe(audio, beam_size=5)
+            for segment in segments:
+                await ws.send_text(segment.text)
+    except WebSocketDisconnect:
+        raise WebSocketDisconnect("WebSocket disconnected")
