e28e7f2cc9b041727358f8d8c0b6cb284ac24ace
yeeeengyu <chldlsrb08@naver.com>
2026-05-09 11:52:14 +0900

    feature: cc_stt 라우팅

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from router import health, cc, cc_stt, summary
+from router import health, cc, summary, cc_stt 
 from pymongo import MongoClient
 
 APP = FastAPI()
