b8d170493d0274eeb32b90e49040ac515f834931
yeeeengyu <chldlsrb08@naver.com>
2026-05-13 19:20:17 +0900

    chore: root라우터 생성

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from router import health, cc, summary, cc_stt 
+from router import health, cc, summary, cc_stt, root
 from pymongo import MongoClient
 
 APP = FastAPI()
+APP.include_router(root.router)
 APP.include_router(health.router)
 APP.include_router(cc.router)
 APP.include_router(cc_stt.router)
-APP.include_router(summary.router)
+# APP.include_router(summary.router)
